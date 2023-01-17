fbb5eb78a0ead5d0a9ab408b1e345a2326cd4e3a
carren <chepkorircarren9@gmail.com>
2023-01-17 15:47:49 +0300

    Made additional changes

--- src/utils/fecthData.js
+++ src/utils/fecthData.js
@@ -2,7 +2,7 @@ export const exerciseOptions = {
   method: "GET",
   headers: {
     "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
-    "X-RapidAPI-Key": process.env.REACT_APP_API_KEY,
+    "X-RapidAPI-Key": "b927409735msh7d3395ecb865ae7p154636jsn68fbc7b2881a",
   },
 };
 
